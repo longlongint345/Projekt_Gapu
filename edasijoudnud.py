163f8274eb0507f6425f73e7611bdab6a0a96926
unknown <laura.raudsepp@gmail.com>
2019-11-11 20:42:56 +0200

    Lõpuks asi liigub aga palju on veel teha.

--- edasijoudnud.py
+++ edasijoudnud.py
@@ -4,21 +4,65 @@ from elements import nupp
 from elements import tekstikast
 
 vajutus = ""
+kirjutatud_tekst = ""
+kirjutamise_järg = -1
+counter = 0
+viga = False
 
 
-def edasi_main(aken, ax, ay, hiir, klikk, klahv):
+def file_to_string(failinimi):
+    sõne = ""
+    with open(failinimi, "r", encoding ="utf-8") as fsisse:
+        for x in fsisse.readlines():
+            tmp = x.strip().split(" ")
+            sõne += " ".join(tmp)
+    return sõne
+
+def edasijõudnud_main(win, wx, wy, hiir, klikk, klahv):
     global vajutus
+    global kirjutatud_tekst
+    global kirjutamise_järg
+    global counter
+    global viga
     vajutus = ""
-    aken.fill((255, 255, 255))
 
-    tagasi_nupp = nupp(0, 0, 100, 100, "tagasi", (0, 255, 0))
+    tekst = file_to_string(os.path.join("jutt", "transkriptsioon.txt"))
+    järgmine_täht = tekst[kirjutamise_järg + 1]  
 
-    if tagasi_nupp.hiire_all(hiir):
-        tagasi_nupp.varv = (255, 0, 0)
-    tagasi_nupp.draw(aken)
-    if tagasi_nupp.is_clicked(klikk, hiir):
+    win.fill((255, 255, 255))
+    kastilaius = 1000
+    kastipikkus = 500
+    kast1 = tekstikast(wx / 2 - kastilaius / 2, 50, kastilaius, kastipikkus)
+    if viga:
+        kast1.äärise_varv = (255, 0, 0)
+    kast1.draw(win)
+    kast1.kuva_tekst(win, tekst)
+
+    # nupud
+    tagasi = nupp(0, 0, 100, 150, "Tagasi", (0, 0, 170), (255, 255, 255))
+    if tagasi.hiire_all(hiir):
+        tagasi.värv = (0, 0, 255)
+    tagasi.draw(win)
+    if tagasi.is_clicked(klikk, hiir):
+        win.fill((0, 0, 0))
         vajutus = "start"
-        aken.fill((0, 0, 0))
+        kirjutatud_tekst = ""
+        kirjutamise_järg = -1
+        counter = 0
         return False
 
+    if klahv != "":
+        kirjutatud_tekst += klahv
+        kirjutamise_järg += 1
+        if tekst[kirjutamise_järg] != kirjutatud_tekst[kirjutamise_järg]:
+            kirjutamise_järg -= 1
+            kirjutatud_tekst = kirjutatud_tekst[:-1]
+            viga = True
+            counter = 0
+            # kirjutamisveale saab reageerida siit
+    kast1.kuva_tekst(win, kirjutatud_tekst, (0, 200, 0))
+    
+
+
     return True
+
